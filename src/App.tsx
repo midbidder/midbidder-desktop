57798fe32e3a4d2278835d85534e364da8307d58
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-01-27 14:14:58 -0600

    Simplified hierarchy.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <h1>Hello, World</h1>
       </header>
     </div>
