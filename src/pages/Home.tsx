828e1547857ce63485dc2511139f383975da03b5
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-03-09 20:01:09 -0600

    Example text swatched remove.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import PlayButton from "../components/PlayButton";
-import { ExampleTextSwatch } from "../components/Text";
 
 export default function Home() {
   return (
     <div>
       <div>
         <PlayButton />
-        <ExampleTextSwatch/>
       </div>
     </div>
   );
